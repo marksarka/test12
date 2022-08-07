07e387164947856948e84a4ba9551b26edf4d281
marksarka <66681979+marksarka@users.noreply.github.com>
2022-08-07 12:35:55 +0300

    Create test.tf

---
+++ test.tf
@@ -0,0 +1,7 @@
+resource "aws_s3_bucket_public_access_block" "positive23" {
+  restrict_public_buckets = true
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls   = false
+  ignore_public_acls  = false
+}
